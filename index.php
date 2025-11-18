9d1b92e9b5fd9e81742c01ef9d8783685da83757
Swanholm Technology <swanholmtech@gmail.com>
2025-11-18 12:59:03 +0100

    save

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center font-bold-italic-dark-bg">Your smart vest has your back</h2>
             <p class="t-center">Revolutionary smart safety technology that protects workers in real-time</p>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Book A Demo</a></p>
           </div>
         </div>
       </div>
@@ -16,7 +16,7 @@
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center font-bold-italic-dark-bg">Seconds Matters</h2>
             <p class="t-center">One life can be save in one second.</p>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Book A Demo</a></p>
           </div>
         </div>
       </div>
@@ -25,28 +25,29 @@
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center font-bold-italic-dark-bg">Be connected Be Safe</h2>
             <p class="t-center">By using Swanholm Alert Vest you can be sure to get help when in need.</p>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Book A Demo</a></p>
           </div>
         </div>
       </div>
     </div>
-    <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+    <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]">
+    </div>
   </div>
 </div>
+<!-- VEST SECTION -->
 <div class="page-banner">
-    <div class="page-banner__content c-dark">
+  <div class="page-banner__content c-dark">
       <h3 class="headline headline--small">AI Fall Detection</h3>
       <h3 class="headline headline--small">Three Alarm Types</h3>
       <h3 class="headline headline--small">Active Printed Light</h3>
       <h3 class="headline headline--small">Swanholm Sense App</h3>
       <a href="#" class="btn btn--large btn--blue">Read More</a>
+    </div>
+    <img src="<?php echo get_theme_file_uri('/images/vest.webp'); ?>" width="300" height="400"/>
   </div>
-  <img src="<?php echo get_theme_file_uri('/images/vest.webp'); ?>" width="300" height="400"/>
-</div>
-
-<div>
   <div>
-    <div class="full-width-split__inner bacground-dark-blue">
+    <div>
+      <div class="full-width-split__inner bacground-dark-blue">
     <div class ="text-amar">
         <div class="event-summary__content" style="flex: 1; padding: 30px 0px 0px 10px">
           <h5 class="event-summary__title headline headline--tiny">€550 B = 3.3% of EU GDP</h5>
@@ -75,7 +76,6 @@
           </p>
         </div>
       </div>
-
       <div class="event-summary">
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny">Solution</h5>
@@ -96,37 +96,6 @@
       </div>
     </div>
   </div>
-  <!-- <div class="full-width-split__two">
-    <div class="full-width-split__inner">
-
-      <div class="event-summary">
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny">€550 B = 3.3% of EU GDP</h5>
-          <p>lost each year to workplace injuries and diseases. (Source: EU-OSHA)</p>
-        </div>
-      </div>
-      <div class="event-summary">
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny">~3,298fatal and ~2.8 M non-fatal</h5>
-          <p>accidents occur annually across the EU. (Source: Eurostat, 2023)</p>
-        </div>
-      </div>
-      <div class="event-summary">
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny">STF are the #2 cause</h5>
-          <p>of workplace accidents — leading to severe, long-term injuries. (Source: Eurostat, 2023)</p>
-        </div>
-      </div>
-      <div class="event-summary">
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny">Lone workers face higher risk</h5>
-          <p>of delayed rescue and undetected incidents.(Source: EU-OSHA, HSE UK)</p>
-        </div>
-      </div>
-
-
-    </div>
-  </div> -->
 </div>
 
 <div class="rio-section">
