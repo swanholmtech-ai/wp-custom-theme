91c47c4417b372772eac5a108e3ef7a90d8aa213
Swanholm Technology <swanholmtech@gmail.com>
2025-11-18 12:31:50 +0100

    save

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
       <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bg-1.webp'); ?>)">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
-            <h2 class="headline headline--medium t-center">Free Transportation</h2>
-            <p class="t-center">All students have free unlimited bus fare.</p>
+            <h2 class="headline headline--medium t-center">What if your Vest could save your life?</h2>
+            <p class="t-center">Revolutionary smart safety technology that protects workers in real-time</p>
             <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
           </div>
         </div>
@@ -14,8 +14,8 @@
       <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bg-2.webp'); ?>)">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
-            <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-            <p class="t-center">Our dentistry program recommends eating apples.</p>
+            <h2 class="headline headline--medium t-center">Seconds Matters</h2>
+            <p class="t-center">One life can be save in one second.</p>
             <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
           </div>
         </div>
@@ -23,8 +23,8 @@
       <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bg-3.webp'); ?>)">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
-            <h2 class="headline headline--medium t-center">Free Food</h2>
-            <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+            <h2 class="headline headline--medium t-center">Be connected Be Safe</h2>
+            <p class="t-center">By using Swanholm Alert Vest you can be sure to get help when in need.</p>
             <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
           </div>
         </div>
